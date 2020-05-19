c15aab9aedc022ffbe6397781d5b416102b7383a
Your Name <eskelinenn.sami@gmail.com>
2020-05-19 16:49:16 +0300

    Tehtävä 8.6

--- Osa8/library-backend/index.js
+++ Osa8/library-backend/index.js
@@ -1,4 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server')
+const uuid = require('uuid/v1')
+
 
 let authors = [
   {
@@ -105,6 +107,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -130,6 +141,18 @@ const resolvers = {
   Author: {
     bookCount: (root) => 
       books.filter(book => book.author === root.name).length
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.find(author => author.name === args.author)) {
+        const author = { name: args.author, id: uuid() }
+        authors = authors.concat(author)
+      }
+
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
+    }
   }
 }
 
