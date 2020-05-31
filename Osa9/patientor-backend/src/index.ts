81301de341454086f317a2e74e81ac26f9a3ea1f
Your Name <eskelinenn.sami@gmail.com>
2020-05-31 11:44:35 +0300

    Exercise 9.10

--- Osa9/patientor-backend/src/index.ts
+++ Osa9/patientor-backend/src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 
+import diagnoseRouter from './routes/diagnoses';
 
+
+const PORT = 3001;
 const app = express();
 
 app.use(express.json());
 app.use(cors())
-
-const PORT = 3001;
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.get('/api/ping', (_req, res) => { 
   console.log('someone pinged here');
---
+++ Osa9/patientor-backend/src/services/diagnoseService.ts
@@ -0,0 +1,11 @@
+import diagnoses from '../../data/diagnoses.json';
+import { Diagnose } from '../types';
+
+
+const getAll = (): Array<Diagnose> => {
+  return diagnoses;
+};
+
+export default {
+  getAll
+};
